a7e41ed18ebca5a9197d2cb407e867e214721450
Yannick Sime <yannicksime05@gmail.com>
2026-01-03 15:13:24 +0100

    Updated main.cpp so that we can pass an input file via command line

--- main.cpp
+++ main.cpp
@@ -2,8 +2,18 @@
 #include "include/parser.h"
 #include <fstream>
 
-int main() {
-    std::ifstream f("../tests_files/operations.txt");
+int main(int argc, char *argv[]) {
+    if(argc < 2) {
+        std::cerr << "Usage: " << argv[0] << "<filename>" << std::endl;
+        return 1;
+    }
+
+    std::ifstream f(argv[1]);
+    if(!f.is_open()) {
+        std::cerr << "Error: Could not open file " << argv[1] << std::endl;
+        return 1;
+    }
+
     Lexer lexer(f);
     std::vector<Token>& tokens = lexer.lex();
     f.close();
